c751e08425f64fc1e99b16a5c9a93dfcec8ac8ce
rahul1-1 <rc9782276591@gmail.com>
2025-06-19 08:40:06 +0530

    Notification System

---
+++ LLD/NotificationSystem/NotificationSystem.java
@@ -0,0 +1,225 @@
+import java.util.*;
+
+interface  INotification {
+    String getContent();
+}
+
+class SimpleNotification implements INotification {
+
+    private  String content;
+
+    public SimpleNotification(String c){
+        this.content = c;
+    }
+
+    @Override
+    public String getContent() {
+        return content;
+    }
+}
+
+abstract class INotificationDecorator implements INotification {
+    protected INotification notification;
+
+    public INotificationDecorator(INotification notification){
+        this.notification = notification;
+    }
+}
+
+
+class TimeStampDecorator extends INotificationDecorator {
+    public TimeStampDecorator(INotification notification) {
+        super(notification);
+    }
+
+    @Override
+    public String getContent() {
+        return new Date() + " - " + notification.getContent();
+    }
+}
+
+class SignatureDecorator extends INotificationDecorator {
+    private String signature;
+
+    public SignatureDecorator(INotification n, String sig) {
+        super(n);
+        this.signature = sig;
+    }
+
+    public String getContent() {
+        return notification.getContent() + "\n-- " + signature + "\n\n";
+    }
+}
+
+interface IObserver {
+    void update();
+}
+
+interface IObservable {
+    void addObserver(IObserver observer);
+    void removeObserver(IObserver observer);
+    void notifyObservers();
+}
+
+class NotificationObservable implements IObservable {
+    private List<IObserver> observers = new ArrayList<>();
+    private INotification currentNotification = null;
+
+    public void addObserver(IObserver obs) {
+        observers.add(obs);
+    }
+
+    public void removeObserver(IObserver obs) {
+        observers.remove(obs);
+    }
+
+    public void notifyObservers() {
+        for (IObserver obs : observers) {
+            obs.update();
+        }
+    }
+
+    public void setNotification(INotification notification) {
+        this.currentNotification = notification;
+        notifyObservers();
+    }
+
+    public INotification getNotification() {
+        return currentNotification;
+    }
+
+    public String getNotificationContent() {
+        return currentNotification.getContent();
+    }
+}
+
+class NotificationService {
+    private NotificationObservable observable;
+    private static NotificationService instance = null;
+    private List<INotification> notifications = new ArrayList<>();
+
+    private NotificationService() {
+        observable = new NotificationObservable();
+    }
+
+    public static NotificationService getInstance() {
+        if (instance == null) {
+            instance = new NotificationService();
+        }
+        return instance;
+    }
+
+    public NotificationObservable getObservable() {
+        return observable;
+    }
+
+    public void sendNotification(INotification notification) {
+        notifications.add(notification);
+        observable.setNotification(notification);
+    }
+}
+
+class Logger implements IObserver {
+    private NotificationObservable notificationObservable;
+
+    public Logger() {
+        this.notificationObservable = NotificationService.getInstance().getObservable();
+        notificationObservable.addObserver(this);
+    }
+
+    public Logger(NotificationObservable observable) {
+        notificationObservable.addObserver(this);
+        this.notificationObservable = observable;
+    }
+
+    public void update() {
+        System.out.println("Logging New Notification : \n" + notificationObservable.getNotificationContent());
+    }
+}
+
+interface INotificationStrategy {
+    void sendNotification(String content);
+}
+
+class EmailStrategy implements INotificationStrategy {
+    private String emailId;
+
+    public EmailStrategy(String emailId) {
+        this.emailId = emailId;
+    }
+    @Override
+    public void sendNotification(String content) {
+
+        System.out.println("Sending email Notification to: " + emailId + "\n" + content);
+    }
+}
+
+class SMSStrategy implements INotificationStrategy {
+    private String mobileNumber;
+
+    public SMSStrategy(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    @Override
+    public void sendNotification(String content) {
+        System.out.println("Sending SMS Notification to: " + mobileNumber + "\n" + content);
+    }
+}
+
+class PopUpStrategy implements INotificationStrategy {
+    public void sendNotification(String content) {
+        System.out.println("Sending Popup Notification: \n" + content);
+    }
+}
+
+class NotificationEngine implements IObserver {
+    private NotificationObservable notificationObservable;
+    private List<INotificationStrategy> notificationStrategies = new ArrayList<>();
+
+    public NotificationEngine() {
+        this.notificationObservable = NotificationService.getInstance().getObservable();
+        notificationObservable.addObserver(this);
+    }
+
+    public NotificationEngine(NotificationObservable observable) {
+        this.notificationObservable = observable;
+    }
+
+    public void addNotificationStrategy(INotificationStrategy ns) {
+        this.notificationStrategies.add(ns);
+    }
+
+    // Can have RemoveNotificationStrategy as well.
+
+    public void update() {
+        String notificationContent = notificationObservable.getNotificationContent();
+        for (INotificationStrategy strategy : notificationStrategies) {
+            strategy.sendNotification(notificationContent);
+        }
+    }
+}
+
+public class NotificationSystem {
+    public static void main(String[] args) {
+
+        // Create NotificationService.
+        NotificationService notificationService = NotificationService.getInstance();
+
+        // Create Logger Observer
+        Logger logger = new Logger();
+
+        // Create NotificationEngine observers.
+        NotificationEngine notificationEngine = new NotificationEngine();
+
+        notificationEngine.addNotificationStrategy(new EmailStrategy("rc.123@gmail.com"));
+        notificationEngine.addNotificationStrategy(new SMSStrategy("+91 9876543210"));
+        notificationEngine.addNotificationStrategy(new PopUpStrategy());
+
+        INotification notification = new SimpleNotification("Your order has been shipped!");
+        notification = new TimeStampDecorator(notification);
+        notification = new SignatureDecorator(notification, "Customer Care");
+
+        notificationService.sendNotification(notification);
+    }
+}
